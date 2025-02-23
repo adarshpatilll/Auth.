c525f61652746a82e314f976b141ecc5f1470be2
Adarsh Patil <adarsh.patil81359@outlook.com>
2025-02-23 22:45:30 +0530

    updated

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const Axios = axios.create({
-	baseURL: "http://localhost:8000/api",
+	baseURL: process.env.BACKEND_URL,
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
