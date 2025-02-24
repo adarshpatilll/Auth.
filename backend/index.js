34c7ae0bfec94b1692f2086c477b21714bdcda03
Adarsh Patil <adarsh.patil81359@outlook.com>
2025-02-24 15:15:10 +0530

    backend default route set in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import errorHandler from "./middlewares/errorHandler.js";
 import router from "./routes/auth.routes.js";
 import startServer from "./server.js";
 
+app.get("/", (req, res) => {
+   res.send("Backend is running successfully! 🚀");
+});
+
 app.use("/api", router);
 
 app.use(errorHandler);
