af091966fe11b5b4020c69f1b91762b12e133cba
Adarsh Patil <77963528+adarshpatilll@users.noreply.github.com>
2025-02-24 17:24:40 +0530

    Update CircularLoader.jsx

--- frontend/src/components/CircularLoader.jsx
+++ frontend/src/components/CircularLoader.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const CircularLoader = ({tailwindcss="border-neutral-100 border-t-neutral-700", size="5"}) => {
+const CircularLoader = ({tailwindcss="border-neutral-100 border-t-neutral-700"}) => {
 	return (
-		<div className={`w-${size} h-${size} ${tailwindcss} border-2 rounded-full animate-spin`}></div>
+		<div className={`w-5 h-5 ${tailwindcss} border-2 rounded-full animate-spin`}></div>
 	);
 };
 
