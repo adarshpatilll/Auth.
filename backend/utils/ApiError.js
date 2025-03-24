a034bcad39ebb93760a23c685032d3b189370717
Adarsh Patil <adarsh.patil81359@outlook.com>
2025-03-24 11:56:12 +0530

    little change in ApiError.js file

--- backend/utils/ApiError.js
+++ backend/utils/ApiError.js
@@ -1,3 +1,14 @@
+/**
+ * @class ApiError
+ * @extends {Error} - Extends the default Error class
+ * @param {number} statusCode - HTTP Status code
+ * @param {string} message - Error message
+ * @param {object} errors - Error details
+ * @param {string} stack - Error stack trace
+ * @description Custom Error class with status code and error details
+ * @example throw new ApiError(400, "Bad Request", { email: "Invalid email" });
+ */
+
 class ApiError extends Error {
    constructor(statusCode, message, errors , stack = "") {
       super(message);
